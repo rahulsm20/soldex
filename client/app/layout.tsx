899b064ba632d5d6ccffe8eb289e252a838d0337
Rahul <rahulsm.1503@gmail.com>
2026-01-10 14:59:38 +0530

    fix(client): add suspense boundary

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -36,7 +37,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <Suspense fallback={<>...</>}>{children}</Suspense>
           <Toaster position="top-center" />
         </ThemeProvider>
       </body>

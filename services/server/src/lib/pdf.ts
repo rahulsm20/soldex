f90cffa1c546faed33ce032e9c530f3002fcdabc
Rahul <rahulsm.1503@gmail.com>
2026-02-01 13:08:09 +0530

    feat: add footer to pdf export

--- services/server/src/lib/pdf.ts
+++ services/server/src/lib/pdf.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import PDFDocument from "pdfkit";
 import { formatDateISO, getRandomColor, renderLineChart } from "shared/utils";
-import { ACCOUNTS_MAP, LOGO_URL } from "shared/utils/constants";
+import { ACCOUNTS, ACCOUNTS_MAP, LOGO_URL } from "shared/utils/constants";
 import interFont from "../../assets/fonts/Inter-VariableFont_opsz,wght.ttf";
 import { getSignedURL, uploadToS3 } from "./s3";
 import {
@@ -44,7 +44,7 @@ export async function renderTransactionsReport(
   req: Request,
   title?: string,
 ): Promise<Buffer> {
-  const { address, limit, offset } = req.query;
+  const { address, limit, page, pageSize, startTime, endTime } = req.query;
   const doc = new PDFDocument();
   const chunks: Uint8Array[] = [];
   const fontPath = path.resolve(
@@ -59,16 +59,36 @@ export async function renderTransactionsReport(
   const response = await fetch(LOGO_URL);
   const logo = await response.arrayBuffer();
   const y = 45;
-
+  let pageNumber = 1;
+  const addFooter = () => {
+    const year = new Date().getFullYear();
+    doc.fontSize(10).text(`© ${year} Soldex`, 20, doc.page.height - 50, {
+      lineBreak: false,
+    });
+    doc
+      .fontSize(6)
+      .text(`Page ${pageNumber}`, doc.page.width - 100, doc.page.height - 50, {
+        lineBreak: false,
+      });
+    pageNumber += 1;
+  };
+  addFooter();
   doc.image(logo, 50, y, { width: 10 });
 
   doc.fontSize(20).text("Soldex", 65, y - 2);
 
-  doc.fontSize(15).text(title || "Transactions Report", { align: "left" });
+  doc.fontSize(12).text(title || "Transactions Report", { align: "left" });
+  doc.moveDown();
+  // Register footer for every page
+  doc.on("pageAdded", addFooter);
+
   const transactions = await getTransactionsUtil({
-    address: address ? String(address) : undefined,
+    address: address as string,
     limit: limit ? Number(limit) : undefined,
-    page: offset ? Number(offset) : undefined,
+    page: page ? Number(page) : undefined,
+    pageSize: pageSize ? Number(pageSize) : undefined,
+    startTime: startTime as string,
+    endTime: endTime as string,
   });
   const chartData = await getTransactionsChartDataUtil(req);
   const values: { label: string; data: number[]; borderColor: string }[] = [];
@@ -76,12 +96,8 @@ export async function renderTransactionsReport(
   const labels = chartData.map((data) => {
     const date = formatDateISO(new Date(data.time));
     for (let key of Object.keys(data)) {
-      // const mapKey = acc;
       if (key !== "time") {
         const mapKey = ACCOUNTS_MAP[key] || key;
-        // if (!values[mapKey]) {
-        //   values[mapKey] = 0;
-        // }
         let entry = values.find((v) => v.label === mapKey);
         if (!entry) {
           entry = {
@@ -91,32 +107,59 @@ export async function renderTransactionsReport(
           };
           values.push(entry);
         }
-        entry.data.push(data?.[key] as number);
-        // values[mapKey] += data?.[key] as number;
+        if (data?.[key]) {
+          entry.data.push(data?.[key] as number);
+        }
       }
     }
     return date;
   });
   await renderLineChart(doc, labels, values);
   doc.moveDown();
-  const tableData: PDFKit.Mixins.TableOptionsWithData["data"] = [
-    ["Date", "Mint Address", "Signature", "Slot", "From", "To"],
-    ...transactions?.transactions.map((tx) => [
+
+  const tokens = ACCOUNTS.map((account) => [account.label, account.sig]);
+  doc.text("Mint Addresses");
+  doc.moveDown();
+  const legendTableData: PDFKit.Mixins.TableOptionsWithData["data"] = [
+    ["Token", "Mint Address"],
+    ...tokens,
+  ];
+
+  doc.fontSize(6).table({ data: legendTableData });
+  doc.moveDown();
+
+  const mappedTransactions =
+    transactions?.transactions?.map((tx, idx) => [
+      (idx + 1).toString(),
       dayjs(tx.blockTime).format("YYYY-MM-DD HH:mm:ss"),
       tx.address ?? "N/A",
       {
         text: tx.signature,
         url: `https://solscan.io/tx/${tx.signature}`,
+        link: `https://solscan.io/tx/${tx.signature}`,
         underline: true,
         color: "blue",
       },
       tx.slot.toString(),
       tx.from_address ?? "N/A",
       tx.to_address ?? "N/A",
-    ]),
+    ]) || [];
+
+  if (mappedTransactions.length === 0) {
+    mappedTransactions.push(["No transactions found", "", "", "", "", "", ""]);
+  }
+
+  doc.text("Transactions");
+  doc.moveDown();
+
+  const tableData: PDFKit.Mixins.TableOptionsWithData["data"] = [
+    ["#", "Date", "Mint Address", "Signature", "Slot", "From", "To"],
+    ...mappedTransactions,
   ];
 
-  doc.fontSize(6).table({ data: tableData });
+  doc.fontSize(6).table({
+    data: tableData,
+  });
 
   doc.end();
 
@@ -125,6 +168,7 @@ export async function renderTransactionsReport(
   return Buffer.concat(chunks);
 }
 
+//--------------------------------------------------------------
 export async function generatePDF(pdfBuffer: Buffer, name?: string) {
   const filename = `${name}.pdf` || `pdfs/${randomUUID()}.pdf`;
 

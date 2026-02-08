73b1d10b777c906e1782c0cb0345751f23899995
Rahul <rahulsm.1503@gmail.com>
2026-02-08 16:23:08 +0530

    fix: attempt fix webhook #2

--- indexer/src/routes/api.ts
+++ indexer/src/routes/api.ts
@@ -11,7 +11,7 @@ router.get("/status", async (_req: Request, res: Response) => {
 router.post("/webhook", async (req: Request, res: Response) => {
   // push event data can be accessed via req.event
   const event = req;
-  console.log(JSON.stringify(event, null, 2));
+  console.log({ event });
   return res.status(200).json({ service: "Indexer API", version: "1.0.0" });
 });
 router.get("*", async (_req: Request, res: Response) => {

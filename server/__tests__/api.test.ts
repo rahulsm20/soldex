7f975bee64d3bda9338f022223f12a70ca885adc
Rahul <rahulsm.1503@gmail.com>
2026-03-02 21:44:41 +0530

    chore: update server/tsconfig.json

--- server/__tests__/api.test.ts
+++ server/__tests__/api.test.ts
@@ -1,12 +1,12 @@
 import { beforeAll, expect, test } from "bun:test";
 import { startServer } from "../src/server";
-
+import { config } from "@/utils/config";
 beforeAll(() => {
   startServer();
 });
 
 test("should return 200 OK", async () => {
-  const response = await fetch("http://localhost:3002");
+  const response = await fetch(`http://localhost:${config.PORT || 3002}`);
   const data = await response.json();
   expect(response.status).toBe(200);
   expect(data).toEqual({ message: "Service running", status: "ok" });
